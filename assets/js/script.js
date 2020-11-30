9ad0f355ceeeb33c5c1a4fc93f18dc0989f75d87
Darren Garcia <darrengarcia159@gmail.com>
2020-11-30 17:26:18 -0600

    new logo and animation to the landing page

--- assets/js/script.js
+++ assets/js/script.js
@@ -117,3 +117,22 @@ savedSearch.on('click', dropdownSelect);
 
 // On first load on page, populate past searches dropdown with terms from local storage
 loadDropdown();
+
+// start of styling for landing page
+
+var mainbox = document.querySelector('#container');
+var text1 = document.querySelector('#text1');
+var text2 = document.querySelector('#text2');
+
+function cursorfollow(e,mainbox)
+{
+    var con = mainbox.getBoundingClientRect();
+    var x = ((e.pageX - con.x)*0.5);
+    text1.style.transform = `translate(${x}px,0)`;
+    text2.style.transform = `translate(${x}px,0)`;
+}
+
+mainbox.addEventListener('mousemove',(e)=>{
+    setInterval(cursorfollow(e,mainbox),1000);
+});
+
